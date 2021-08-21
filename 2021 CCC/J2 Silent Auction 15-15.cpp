635c0014d8d4cc14f3f01272b816dc33530a416e
Alim-random <89221797+Alim-random@users.noreply.github.com>
2021-08-20 23:50:35 -0400

    Update J2 Silent Auction 15-15.cpp

--- 2021 CCC/J2 Silent Auction 15-15.cpp
+++ 2021 CCC/J2 Silent Auction 15-15.cpp
@@ -4,32 +4,39 @@ using namespace std;
 
 int main() {
 
+	
+	
+//Get input of how many people
 int a;
 cin >> a;
-const int ppl = a;
+const int ppl = a; //use "const" since it is necessary for initializing arrays
 
 string name[ppl];
 int amount[ppl];
 
+//Input for people and the amount that they bid
 for(int i = 0; i<ppl; i++){
 	cin >> name[i];
 	cin >> amount[i];
 
 }
 
+//Finding max amount
 int maxAmm = 0;
 
 for(int i = 0; i<ppl; i++){
-	if(amount[i]>maxAmm){
+	if(amount[i]>maxAmm){ //manually initializing max amount
 		maxAmm = amount[i];
 	}
 
 }
 
+//Getting the first max amount and output that
+//since it is first and highest bid who wins
 for(int i = 0; i<ppl; i++){
 	if(amount[i] == maxAmm){
 		cout << name[i] << endl;
-		break;
+		break; //break ends the loop
 	}
 }
 
