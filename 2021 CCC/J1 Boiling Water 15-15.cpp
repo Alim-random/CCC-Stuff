a67ec45d1c7fd30b1af10b4f3c5c9f34c2d8f3a8
Alim-random <89221797+Alim-random@users.noreply.github.com>
2021-08-20 23:45:21 -0400

    Update J1 Boiling Water 15-15.cpp

--- 2021 CCC/J1 Boiling Water 15-15.cpp
+++ 2021 CCC/J1 Boiling Water 15-15.cpp
@@ -3,11 +3,21 @@
 using namespace std;
 
 int main() {
+/*Analysis:
+ *Very simple code, I have the information of if the amount that
+ *I output is more than 100, then output a -1 since it is below,
+ *and the same for the other two possibilies with 0, and 1 based
+ *on the output
+ */
 
+//Getting input
 	int input;
 	cin >> input;
 
+//Formula provided:
 	input = (5*input) - 400;
+	
+//Main code to get output
 	if(input>100){
 		cout << input << endl;
 		cout << "-1" << endl;
